5006c6792485c7a65e0ae4f5723cc52ea4ca56f3
Christof Musik <christof@senfdax.de>
2014-08-17 12:56:15 +0200

    just install webapp package and don't handle apache

--- manifests/webapp.pp
+++ manifests/webapp.pp
@@ -1,7 +1,3 @@
 class zarafa::webapp {
-  contain zarafa::repo
   contain zarafa::webapp::install
-  contain zarafa::webapp::service
-
-  Class["zarafa::repo"] -> Class["zarafa::webapp::install"] -> Class["zarafa::webapp::service"]
 }
--- manifests/webapp/service.pp
+++
@@ -1,5 +0,0 @@
-class zarafa::webapp::service {
-  service { "httpd":
-    ensure => running,
-  }
-}
