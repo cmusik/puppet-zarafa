5310b9a18b39527e2f69f8ba9008ed1cc7d69cf6
Christof Musik <christof@senfdax.de>
2014-08-17 13:00:01 +0200

    remove repo handling

--- manifests/repo.pp
+++
@@ -1,8 +0,0 @@
-class zarafa::repo {
-  yumrepo { "yaffas":
-    baseurl => "http://repo.yaffas.org/releases/latest/rhel/6",
-    descr => "yaffas repository",
-    enabled => 1,
-    gpgcheck => 0
-  }
-}
